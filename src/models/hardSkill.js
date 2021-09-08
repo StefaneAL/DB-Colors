05e3b63379ebb72807aaa1d88861fbe1ed16d508
Stefane Almeida <stefanedealmeida01@gmail.com>
2021-09-08 09:46:00 -0300

    voltando model ao padrao original

--- src/models/hardSkill.js
+++ src/models/hardSkill.js
@@ -14,11 +14,7 @@ const hardSkillSchema = new mongoose.Schema({
         type: Date,
         require: true,
         default: new Date
-    },
-    users:[{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user"
-    }]
+    }
 },{
     versionKey: false
 })
